ed6ebdc4260e13adffe2d77d723ebc28fe11b12e
Abdullah Alsharif <gxr199@gmail.com>
2017-12-12 13:28:26 +0000

    Bootstrapper of my code

---
+++ R/ConcentroR-Bootstrap.R
@@ -0,0 +1,7 @@
+ana <- ragtag::read_analysis()
+mut <- ragtag::read_mutants()
+
+ragtag::table_generator_coverage_others((ana %>% filter(datagenerator != "dravm")), m = "mean")
+ragtag::table_generator_timing_others_nonRand((ana %>% filter(datagenerator != "dravm")), m = "mean")
+ragtag::table_generator_mutation_score_others_nonRand(mut, m = "mean")
+ragtag::concentro_table_combaining(ana, mut, mm = "mean")
